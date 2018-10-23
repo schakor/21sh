2b73f029cffae94a77cfbf5524ed332f60c7cbef
Khan SADIRAC <khsadira@e1r2p19.42.fr>
2018-10-23 15:01:00 +0200

    prompt

--- srcs/terminal/display_prompt.c
+++ srcs/terminal/display_prompt.c
@@ -6,7 +6,7 @@
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/15 23:03:00 by schakor           #+#    #+#             */
-/*   Updated: 2018/10/15 23:03:24 by schakor          ###   ########.fr       */
+/*   Updated: 2018/10/23 14:59:45 by khsadira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 void		display_prompt(void)
 {
-	ft_putstr("$>");
+	char	*cwd;
+	char	buff[4097];
+	int		i;
+	int		len;
+	char	*tmp;
+
+	tmp = NULL;
+	if ((cwd = getcwd(buff, 4096)))
+	{
+		len = ft_strlen(cwd);
+		i = len;
+		while (i > 0 && cwd[i] != '/')
+			i--;
+		if (cwd[i] == '/' && ft_strlen(cwd) != 1)
+			i++;
+		tmp = ft_strsub(cwd, i, len - i);
+	}
+	ft_putstr("\033[0;31m");
+	ft_putstr("<");
+	if (tmp)
+		ft_putstr(tmp);
+	ft_putstr("> ");
+	ft_putstr("\033[0m");
+	ft_strdel(&tmp);
 }
